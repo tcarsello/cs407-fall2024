38609475c4d38bc6cf16a29cf41b482197c75bcc
Thomas Carsello <tkcarsello@gmail.com>
2024-09-16 15:51:10 -0400

    Delete courses

--- frontend/src/components/course/CourseDetails.js
+++ frontend/src/components/course/CourseDetails.js
@@ -1,11 +1,49 @@
 import { useState } from 'react'
+import { useAuthContext } from '../../hooks/UseAuthContext'
 
-const CourseDetails = ({ course }) => {
+import ConfirmDialog from '../ConfirmDialog'
+
+import { GrTrash } from 'react-icons/gr'
+
+const CourseDetails = ({ course, onDelete }) => {
+
+    const { user } = useAuthContext()
+
+    const [deleteCoursePopupEnabled, setDeleteCoursePopupEnabled] = useState(false)
+
+    const handleDeleteCourse = async () => {
+        const response = await fetch(`/api/course/${course.courseId}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${user.token}`
+            }
+        })
+        if (response.ok) {
+            onDelete()
+        }
+    }
 
     return (
         <div className='course-details'>
-            <h2>{course.courseName}</h2>
-            <p>{course.courseDescription}</p>
+            <div className='flex' style={{alignItems: 'center'}}>
+                <h2>{course.courseName}</h2>
+                {course.coordinatorId == user.userId && <GrTrash style={{width: '50px'}} onClick={() => {setDeleteCoursePopupEnabled(true)}}/>}
+            </div>
+            <span>{course.courseDescription}</span>
+
+            {deleteCoursePopupEnabled &&
+                <ConfirmDialog
+                    text='Are you sure you want to delete this course??'
+                    isOpen={deleteCoursePopupEnabled}
+                    onClose={() => setDeleteCoursePopupEnabled(false) }
+                    onConfirm={() => {
+                        setDeleteCoursePopupEnabled(false)
+                        handleDeleteCourse()
+                    }}
+                />
+            }
+
         </div>
     )
     
