bebd38db90f885c193468329933d8c76d4953f1f
Jason Bodzy <jasonbodzy@gmail.com>
2024-09-24 15:20:37 -0400

    display context

---
+++ frontend/src/context/DisplayContext.js
@@ -0,0 +1,45 @@
+import { useState, useEffect, createContext, useContext } from 'react'
+import { useParams } from 'react-router-dom'
+import { useAuthContext } from '../hooks/UseAuthContext'
+
+export const DisplayContext = createContext()
+
+export const useDisplayContext = () => {
+    return useContext(DisplayContext)
+}
+
+export const DisplayContextProvider = ({ children }) => {
+
+    const { user } = useAuthContext()
+
+    useEffect(() => {
+
+        const getClassNames = () => {
+            if (user.lightMode) {
+                return {
+                    settingsCard : 'settings-card',
+                    settingsPageContainer: 'settings-page-container',
+                    standardFormInput: 'standard-form-input',
+                    text: 'light-text',
+                    button: 'standard-button light-button-scheme'
+                }
+            }
+    
+            return {
+                settingsCard : 'settings-card foreground-dark',
+                settingsPageContainer: 'settings-page-container background-dark',
+                standardFormInput: 'standard-form-input emphasis-dark',
+                text: 'dark-text',
+                button: 'standard-button dark-button-scheme'
+            }
+        }
+
+    }, [])
+
+    return (
+        <DisplayContext.Provider value={{...state}}>
+            {children}
+        </DisplayContext.Provider>
+    )
+
+}
