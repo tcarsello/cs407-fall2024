ee1452c9532d4cd954c2308b8682c5c2567d32ee
Zachary Heskett <zheskett@gmail.com>
2024-09-29 02:15:22 -0400

    reset formatting

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,63 +1,65 @@
-require("dotenv").config();
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
-const nodemailer = require("nodemailer");
-const Buffer = require("buffer").Buffer;
-const path = require("path");
+require('dotenv').config()
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const nodemailer = require('nodemailer')
+const Buffer = require('buffer').Buffer
+const path = require('path')
 
-const sequelize = require("../database");
-const s3 = require("../objectstore");
-const { Sequelize } = require("sequelize");
+const sequelize = require('../database')
+const s3 = require('../objectstore')
+const { Sequelize } = require('sequelize')
 
-const User = require("../models/userModel");
-const Course = require("../models/courseModel");
-const CourseInvite = require("../models/courseInviteModel");
+const User = require('../models/userModel')
+const Course = require('../models/courseModel')
+const CourseInvite = require('../models/courseInviteModel')
 
-const { createJWT } = require("../utils");
+const { createJWT } = require('../utils')
 
 const genSalt = async () => {
-	const salt = await bcrypt.genSalt(5);
-	return salt;
-};
+    const salt = await bcrypt.genSalt(5)
+    return salt
+}
 
 const createUser = async (req, res) => {
-	try {
-		const { email, firstName, lastName, password } = req.body;
+    try {
 
-		if (!email) throw "Email must be provided";
-		if (!firstName) throw "First Name must be provided";
-		if (!lastName) throw "Last Name must be provided";
-		if (!password) throw "Password must be provided";
+        const { email, firstName, lastName, password } = req.body
 
-		if (password.length < 6) throw "Password must be at least 6 characers";
+        if (!email) throw "Email must be provided"
+        if (!firstName) throw "First Name must be provided"
+        if (!lastName) throw "Last Name must be provided"
+        if (!password) throw "Password must be provided"
 
-		const queryUser = await User.findOne({
-			where: {
-				email,
-			},
-		});
-		if (queryUser) throw "A user already exists with that email address";
+        if (password.length < 6) throw "Password must be at least 6 characers"
 
-		const salt = await genSalt();
-		const passwordHash = await bcrypt.hash(password, salt);
+        const queryUser = await User.findOne({
+            where: {
+                email
+            }
+        })
+        if (queryUser) throw "A user already exists with that email address"
 
-		const user = await User.create({
-			email,
-			firstName,
-			lastName,
-			passwordHash,
-		});
-		const token = createJWT(user.dataValues.userId);
+        const salt = await genSalt()
+        const passwordHash = await bcrypt.hash(password, salt)
 
-		res.status(200).json({
-			...user.dataValues,
-			token,
-		});
-	} catch (err) {
-		console.error(err);
-		res.status(400).json({ error: err });
-	}
-};
+        const user = await User.create({
+            email,
+            firstName,
+            lastName,
+            passwordHash,
+        })
+        const token = createJWT(user.dataValues.userId)
+
+        res.status(200).json({
+            ...user.dataValues,
+            token,
+        })
+
+    } catch (err) {
+        console.error(err)
+        res.status(400).json({ error: err })
+    }
+}
 
 const forgotPassword = async (req, res) => {
 	try {
@@ -109,162 +111,175 @@ const forgotPassword = async (req, res) => {
 };
 
 const loginUser = async (req, res) => {
-	try {
-		const { email, password } = req.body;
+    try {
 
-		if (!email) throw "Email must be provided";
-		if (!password) throw "Password must be provided";
+        const { email, password } = req.body
 
-		const user = await User.findOne({
-			where: {
-				email,
-			},
-		});
-		if (!user) throw "Invalid credentials";
+        if (!email) throw "Email must be provided"
+        if (!password) throw "Password must be provided"
 
-		const match = await bcrypt.compare(password, user.dataValues.passwordHash);
-		if (!match) throw "Invalid credentials";
+        const user = await User.findOne({
+            where: {
+                email
+            }
+        })
+        if (!user) throw "Invalid credentials"
 
-		const token = createJWT(user.dataValues.userId);
+        const match = await bcrypt.compare(password, user.dataValues.passwordHash)
+        if (!match) throw "Invalid credentials"
 
-		res.status(200).json({
-			...user.dataValues,
-			token,
-		});
-	} catch (err) {
-		console.error(err);
-		res.status(400).json({ error: err });
-	}
-};
+        const token = createJWT(user.dataValues.userId)
+
+        res.status(200).json({
+            ...user.dataValues,
+            token,
+        })
+
+    } catch (err) {
+        console.error(err)
+        res.status(400).json({ error: err })
+    }
+}
 
 const getUser = async (req, res) => {
-	try {
-		const { userId } = req.params;
+    try {
 
-		if (userId != req.user.userId) throw "Wrong user";
+        const { userId } = req.params
 
-		const user = await User.findOne({
-			where: {
-				userId,
-			},
-		});
+        if (userId != req.user.userId) throw "Wrong user"
 
-		res.status(200).json(user.dataValues);
-	} catch (err) {
-		console.error(err);
-		res.status(400).json({ error: err });
-	}
-};
+        const user = await User.findOne({
+            where: {
+                userId
+            }
+        })
+
+        res.status(200).json(user.dataValues)
+
+    } catch (err) {
+        console.error(err)
+        res.status(400).json({ error: err })
+    }
+}
 
 const deleteUser = async (req, res) => {
-	try {
-		const { userId } = req.params;
+    try {
 
-		if (userId != req.user.userId) throw "Wrong user";
+        const { userId } = req.params
 
-		await User.destroy({
-			where: {
-				userId,
-			},
-		});
+        if (userId != req.user.userId) throw "Wrong user"
 
-		res.status(200).json({
-			message: "User deleted",
-		});
-	} catch (err) {
-		console.error(err);
-		res.status(400).json({ error: err });
-	}
-};
+        await User.destroy({
+            where: {
+                userId
+            }
+        })
+
+        res.status(200).json({
+            message: "User deleted"
+        })
+
+    } catch (err) {
+        console.error(err)
+        res.status(400).json({ error: err })
+    }
+}
 
 const updateUser = async (req, res) => {
-	try {
-		const { userId } = req.params;
-		const { email, firstName, lastName, password, lightMode } = req.body;
+    try {
 
-		if (userId != req.user.userId) throw "Wrong user";
+        const { userId } = req.params
+        const { email, firstName, lastName, password, lightMode } = req.body
 
-		let passwordHash = undefined;
-		if (password) {
-			if (password.length < 6) throw "Password must be at least 6 characters!";
-			const salt = await genSalt();
-			passwordHash = await bcrypt.hash(password, salt);
-		}
+        if (userId != req.user.userId) throw "Wrong user"
 
-		await User.update(
-			{
-				email,
-				firstName,
-				lastName,
-				passwordHash,
-				lightMode,
-			},
-			{
-				where: {
-					userId,
-				},
-			}
-		);
+        let passwordHash = undefined
+        if (password) {
+            if (password.length < 6) throw 'Password must be at least 6 characters!'
+            const salt = await genSalt()
+            passwordHash = await bcrypt.hash(password, salt)
+        }
 
-		const user = await User.findOne({
-			where: {
-				userId,
-			},
-		});
-		if (!user) throw "User not found";
+        await User.update(
+            {
+                email,
+                firstName,
+                lastName,
+                passwordHash,
+                lightMode
+            },
+            {
+                where: {
+                    userId
+                }
+            }
+        )
 
-		const token = createJWT(user.dataValues.userId);
+        const user = await User.findOne({
+            where: {
+                userId
+            }
+        })
+        if (!user) throw "User not found"
 
-		res.status(200).json({ ...user.dataValues, token });
-	} catch (err) {
-		console.error(err);
-		res.status(400).json({ error: err });
-	}
-};
+        const token = createJWT(user.dataValues.userId)
+
+        res.status(200).json({ ...user.dataValues, token })
+
+    } catch (err) {
+        console.error(err)
+        res.status(400).json({ error: err })
+    }
+}
 
 const resetUserPassword = async (req, res) => {
-	try {
-		res.status(200).json();
-	} catch (err) {
-		console.error(err);
-		res.status(400).json({ error: err });
-	}
-};
+    try {
+
+        res.status(200).json()
+
+    } catch (err) {
+        console.error(err)
+        res.status(400).json({ error: err })
+    }
+}
 
 const getCoordinatingCourses = async (req, res) => {
-	try {
-		const { userId } = req.params;
+    try {
 
-		if (userId != req.user.userId) throw "Wrong user";
+        const { userId } = req.params
 
-		const courses = await Course.findAll({
-			where: {
-				coordinatorId: req.user.userId,
-			},
-			order: [["createdAt", "DESC"]],
-		});
+        if (userId != req.user.userId) throw "Wrong user"
 
-		res.status(200).json({ courses });
-	} catch (err) {
-		console.error(err);
-		res.status(400).json({ error: err });
-	}
-};
+        const courses = await Course.findAll({
+            where: {
+                coordinatorId: req.user.userId
+            },
+            order: [['createdAt', 'DESC']]
+        })
+
+        res.status(200).json({ courses })
+
+    } catch (err) {
+        console.error(err)
+        res.status(400).json({ error: err })
+    }
+}
 
 const getInvites = async (req, res) => {
-	try {
-		const { userId } = req.params;
+    try {
 
-		if (userId != req.user.userId) throw "Wrong user";
+        const { userId } = req.params
 
-		const user = await User.findOne({
-			where: {
-				userId,
-			},
-		});
-		if (!user) throw "User does not exist";
+        if (userId != req.user.userId) throw "Wrong user"
 
-		const queryString = `
+        const user = await User.findOne({
+            where: {
+                userId
+            }
+        })
+        if (!user) throw "User does not exist"
+
+        const queryString = `
             SELECT
                 c.*,
                 ci.*
@@ -277,147 +292,157 @@ const getInvites = async (req, res) => {
             WHERE
                 u.email=:email
             ;
-        `;
+        `
 
-		const invites = await sequelize.query(queryString, {
-			replacements: {
-				email: user.email,
-			},
-			type: Sequelize.QueryTypes.SELECT,
-		});
+        const invites = await sequelize.query(queryString, {
+            replacements: {
+                email: user.email
+            },
+            type: Sequelize.QueryTypes.SELECT
+        })
 
-		res.status(200).json({ invites });
-	} catch (err) {
-		console.error(err);
-		res.status(400).json({ error: err });
-	}
-};
+        res.status(200).json({ invites })
+
+    } catch (err) {
+        console.error(err)
+        res.status(400).json({ error: err })
+    }
+}
 
 const getJoinedCourses = async (req, res) => {
-	try {
-		const { userId } = req.params;
+    try {
 
-		const user = await User.findOne({ where: { userId } });
-		if (!user) throw "User does not exist";
+        const { userId } = req.params
 
-		const courses = await user.getCourses();
+        const user = await User.findOne({ where: { userId } })
+        if (!user) throw "User does not exist"
 
-		res.status(200).json({ courses });
-	} catch (err) {
-		console.error(err);
-		res.status(400).json({ error: err });
-	}
-};
+        const courses = await user.getCourses()
+
+        res.status(200).json({ courses })
+
+    } catch (err) {
+        console.error(err)
+        res.status(400).json({ error: err })
+    }
+}
 
 const uploadProfilePicture = async (req, res) => {
-	try {
-		const { userId } = req.params;
-		const { mimeType, imageBase64 } = req.body;
+    try {
 
-		if (!mimeType) throw "Mime type must be provided";
-		if (!imageBase64) throw "Image base64 must be provided";
+        const { userId } = req.params
+        const { mimeType, imageBase64 } = req.body
 
-		let fileExt = mimeType.split("/")[1].toLowerCase();
+        if (!mimeType) throw "Mime type must be provided"
+        if (!imageBase64) throw 'Image base64 must be provided'
 
-		if (fileExt === "jpg") fileExt = "jpeg";
-		if (!["jpeg", "png"].includes(fileExt)) throw "File extension must be jpeg or png";
+        let fileExt = mimeType.split('/')[1].toLowerCase()
 
-		const buffer = Buffer.from(imageBase64, "base64");
+        if (fileExt === 'jpg') fileExt = 'jpeg'
+        if (!['jpeg', 'png'].includes(fileExt)) throw "File extension must be jpeg or png"
 
-		const params = {
-			Bucket: process.env.AWS_S3_BUCKET_NAME,
-			Key: `profile_pictures/${userId}_pfp.${fileExt}`,
-			Body: buffer,
-			ContentType: mimeType,
-		};
-		const data = await s3.upload(params).promise();
+        const buffer = Buffer.from(imageBase64, 'base64')
 
-		await User.update(
-			{
-				pfpFileType: fileExt,
-			},
-			{
-				where: {
-					userId,
-				},
-			}
-		);
+        const params = {
+            Bucket: process.env.AWS_S3_BUCKET_NAME,
+            Key: `profile_pictures/${userId}_pfp.${fileExt}`,
+            Body: buffer,
+            ContentType: mimeType
+        }
+        const data = await s3.upload(params).promise()
 
-		res.status(200).json({
-			message: "Profile picture uploaded successfully",
-			imageUrl: data.Location,
-		});
-	} catch (err) {
-		console.error(err);
-		res.status(400).json({ error: err });
-	}
-};
+        await User.update(
+            {
+                pfpFileType: fileExt
+            },
+            {
+                where: {
+                    userId
+                }
+            }
+        )
+
+        res.status(200).json({
+            message: 'Profile picture uploaded successfully',
+            imageUrl: data.Location
+        })
+
+    } catch (err) {
+        console.error(err)
+        res.status(400).json({ error: err })
+    }
+}
 
 const getProfilePicture = async (req, res) => {
-	try {
-		const { userId } = req.params;
+    try {
 
-		const user = await User.findOne({
-			where: {
-				userId,
-			},
-		});
-		if (!user) throw "User not found";
+        const { userId } = req.params
 
-		if (!user.pfpFileType) {
-			const defaultImagePath = path.join(__dirname, "../static/default-pfp.jpeg");
-			res.sendFile(defaultImagePath);
-			return;
-		}
+        const user = await User.findOne({
+            where: {
+                userId
+            }
+        })
+        if (!user) throw "User not found"
 
-		const params = {
-			Bucket: process.env.AWS_S3_BUCKET_NAME,
-			Key: `profile_pictures/${userId}_pfp.${user.pfpFileType}`,
-		};
+        if (!user.pfpFileType) {
 
-		const data = await s3.getObject(params).promise();
+            const defaultImagePath = path.join(__dirname, '../static/default-pfp.jpeg')
+            res.sendFile(defaultImagePath)
+            return
 
-		res.setHeader("Content-Type", data.ContentType || `image/${user.pfpFileType}`);
-		res.send(data.Body);
-	} catch (err) {
-		console.error(err);
-		res.status(400).json({ error: err });
-	}
-};
+        }
+
+        const params = {
+            Bucket: process.env.AWS_S3_BUCKET_NAME,
+            Key: `profile_pictures/${userId}_pfp.${user.pfpFileType}`
+        }
+
+        const data = await s3.getObject(params).promise()
+
+        res.setHeader('Content-Type', data.ContentType || `image/${user.pfpFileType}`)
+        res.send(data.Body)
+
+    } catch (err) {
+        console.error(err)
+        res.status(400).json({ error: err })
+    }
+}
 
 const getUserPublicInfo = async (req, res) => {
-	try {
-		const { userId } = req.params;
+    try {
 
-		const user = await User.findOne({
-			where: { userId },
-		});
+        const { userId } = req.params
 
-		if (!user) throw "User not found";
+        const user = await User.findOne({
+            where: { userId }
+        })
 
-		res.status(200).json({
-			userId: user.userId,
-			firstName: user.firstName,
-			lastName: user.lastName,
-		});
-	} catch (err) {
-		console.error(err);
-		res.status(400).json({ error: err });
-	}
-};
+        if (!user) throw "User not found"
+
+        res.status(200).json({
+            userId: user.userId,
+            firstName: user.firstName,
+            lastName: user.lastName
+        })
+
+    } catch (err) {
+        console.error(err)
+        res.status(400).json({ error: err })
+    }
+}
 
 module.exports = {
-	createUser,
-	forgotPassword,
-	loginUser,
-	getUser,
-	deleteUser,
-	updateUser,
-	resetUserPassword,
-	getCoordinatingCourses,
-	getInvites,
-	getJoinedCourses,
-	uploadProfilePicture,
-	getProfilePicture,
-	getUserPublicInfo,
-};
+    createUser,
+    loginUser,
+    getUser,
+    deleteUser,
+    updateUser,
+    resetUserPassword,
+    getCoordinatingCourses,
+    getInvites,
+    getJoinedCourses,
+    uploadProfilePicture,
+    getProfilePicture,
+    getUserPublicInfo
+}
