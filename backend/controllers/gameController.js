2d209de4f7fe9907202fb13c53020fccba9e2fe0
Zachary Heskett <zheskett@gmail.com>
2024-10-27 16:58:45 -0400

    Resign endpoint

--- backend/controllers/gameController.js
+++ backend/controllers/gameController.js
@@ -1,3 +1,4 @@
+const { Sequelize, Op } = require('sequelize')
 const Game = require('../models/gameModel')
 
 const getGame = async (req, res) => {
@@ -30,8 +31,46 @@ const deleteGame = async (req, res) => {
     }
 }
 
+const resignGame = async (req, res) => {
+	try {
+		const { gameId } = req.params;
+		const { userId } = req.body;
+
+		if (userId != req.user.userId) throw "Wrong user";
+
+		const game = await Game.findOne({
+			where: {
+				gameId,
+				[Op.or]: {
+					playerOneId: userId,
+					playerTwoId: userId,
+				},
+			},
+		});
+
+		if (!game) {
+			throw "Game not found";
+		}
+
+		if (game.status !== "New" && game.status !== "In Progress") {
+			throw "Game already ended";
+		}
+
+		const new_status = game.playerOneId === userId ? "Player Two Win" : "Player One Win";
+
+		await game.update({
+			status: new_status,
+		});
+		res.status(200).json(game);
+	} catch (err) {
+		console.error(err);
+		res.status(400).json({ error: err });
+	}
+};
+
 module.exports = {
     getGame,
     updateGame,
-    deleteGame
+    deleteGame,
+    resignGame
 }
--- backend/routes/gameRoutes.js
+++ backend/routes/gameRoutes.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const requireAuth = require('../middleware/requireAuth')
 
-const { getGame, updateGame, deleteGame } = require('../controllers/gameController')
+const { getGame, updateGame, deleteGame, resignGame } = require('../controllers/gameController')
 
 const router = express.Router()
 
 router.get('/:gameId', requireAuth, getGame)
 router.patch('/:gameId', requireAuth, updateGame)
 router.delete('/:gameId', requireAuth, deleteGame)
+
+router.post('/:gameId/resign', requireAuth, resignGame);
 
 module.exports = router
